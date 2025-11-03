e82e243f804dfffa2e41d2b5d42230f55e651221
crazyt0mmy <tommaso.baldo008@gmail.com>
2025-11-04 00:01:53 +0100

    Update thread.cpp

--- thread.cpp
+++ thread.cpp
@@ -12,14 +12,16 @@ int TASK = 100;
 int WORKER = 5;
 
 struct task{
-   
+    int id;
+    task(int i) : id(i) {} 
 };
    
 void crea_coda(queue<task> coda){
-    for(int i; i < TASK; i++){
+    for(int i = 0; i < TASK; i++){
         coda.emplace(i);
     }
    
+    
 }
 
 void lavora(){
@@ -29,5 +31,6 @@ void lavora(){
 int main(){
     queue<task> coda;
     crea_coda(coda);
+
 
 }
