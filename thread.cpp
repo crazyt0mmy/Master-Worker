13728913fd316f40d93a72aa26957ea3944a93d8
crazyt0mmy <tommaso.baldo008@gmail.com>
2025-11-09 00:19:58 +0100

    Update thread.cpp

--- thread.cpp
+++ thread.cpp
@@ -1,36 +1,51 @@
 #include <iostream>
-#include <mutex>
-#include <chrono>
 #include <thread>
-#include <string>
-#include <vector>
+#include <mutex>
 #include <queue>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <chrono>
+#include <future>
+#include <atomic>
 
 using namespace std;
 
-int TASK = 100;
-int WORKER = 5;
+int N_TASK = 100;
+int N_WORKER = 5;
+int T_MAX = 4;
 
-struct task{
-    int id;
-    task(int i) : id(i) {} 
-};
-   
-void crea_coda(queue<task> coda){
-    for(int i = 0; i < TASK; i++){
-        coda.emplace(i);
+// Genera numero casuale
+int trandom() {
+    static bool inizializzato = false;
+    if (!inizializzato) {
+        srand(time(nullptr));   // Inizializza il generatore SOLO la prima volta
+        inizializzato = true;
     }
-   
-    
+    return (rand() % T_MAX) + 1;
 }
+// Creo struct
+struct TASK {
+    int id;
+    int tempo_esecuzione;
+};
 
-void lavora(){
+struct WORKER {
+    int id;
    
+};
+ 
+// Funzione per creare la coda
+void crea_coda(queue<TASK>& coda){
+    for(int i = 0; i < N_TASK; i++){
+        TASK t;                                         // Istanzio la struct
+        t.id = i;                                       // Imposto l'ID
+        t.tempo_esecuzione = trandom();                 // Imposto il tempo di esecuzione randomicamente
+        coda.push(t);                                   // Pusho la task nella coda
+    }
 }
 
 int main(){
-    queue<task> coda;
-    crea_coda(coda);
-
-
+    queue<TASK> coda;   // Creo coda
+    crea_coda(coda);    // Riempio la coda di task
 }
